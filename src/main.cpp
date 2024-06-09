d6c5cbee468f8accb6a3a0fa4e7a59362b7e8496
mohamed arish <arish1248@gmail.com>
2024-06-09 23:02:48 +0530

    mend

--- src/main.cpp
+++ src/main.cpp
@@ -47,7 +47,7 @@ bool shell_loop(Prompt& prompt) {
 
 std::string exec(const std::string& command) {
     std::array<char, 128> buffer;
-    std::string result;
+    std::string result{};
     std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(command.c_str(), "r"),
                                                   pclose);
     if (!pipe) {
